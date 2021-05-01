fce715dc2f11abbec00c2970fdc2a7c99e37d799
Sourav Sarker <souravsker@gmail.com>
2021-05-01 20:06:54 -0230

    reduce practice --> min

--- reduce.js
+++ reduce.js
@@ -1,8 +1,12 @@
 const aList = [1, 2, 5, 9, 6, 25, 15, 14]
 
+// Max Value
 const biggest = aList.reduce((acc, val) => val > acc ? val : acc, 0)
-
 console.log(`max value: ${biggest}`)
+
+// Min Value
+const smallest = aList.reduce((acc, val) => val < acc ? val : acc)
+console.log(`max value: ${smallest}`)
 
 // average
 const average = aList.reduce((acc, val) => acc + val, 0) / aList.length
